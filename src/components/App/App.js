bb4456d90f1a4ed018a76675a6db513ad8984f91
Stanislav Bakryu <stasbakryu@gmail.com>
2021-04-06 00:00:54 +0300

    add functional table

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import Table from "../Table";
 import defaultData from "../../services/getData";
@@ -6,19 +6,52 @@ import "./app.css";
 
 function App() {
   const [tableData, setTableData] = useState(defaultData);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [startPoints, setStartPoints] = useState({
+    day: null,
+    hour: null,
+    isFree: true,
+  });
+
+  const changeTableData = (currentDay, currentHour) => {
+    const { day, hour, isFree } = startPoints;
+    let startDay = day <= currentDay ? day : currentDay;
+    const endDay = day >= currentDay ? day : currentDay;
+    let startHour = hour <= currentHour ? hour : currentHour;
+    const endHour = hour >= currentHour ? hour : currentHour;
+    let dayCount = 1;
+    const newTableData = { ...tableData };
+    for (const dataItem in newTableData) {
+      if (startDay >= dayCount) {
+        for (startHour; startHour <= endHour; startHour++) {
+          newTableData[dataItem][startHour] = isFree;
+          console.log(startDay <= dayCount);
+        }
+      }
+      ++dayCount;
+    }
+    setTableData(newTableData);
+  };
 
   const toggleCell = (row, column) => {
     setTableData((prevState) => {
       const currentRow = [...prevState[row]];
       currentRow[column] = !currentRow[column];
-      // console.log(currentRow);
       return { ...prevState, [row]: currentRow };
     });
   };
-  // console.log(tableData);
+  // console.log(controlPoints, isMouseDown);
   return (
     <div className="App">
-      <Table tableData={tableData} toggleCell={toggleCell} />
+      <Table
+        tableData={tableData}
+        toggleCell={toggleCell}
+        isMouseDown={isMouseDown}
+        setIsMouseDown={setIsMouseDown}
+        startPoints={startPoints}
+        setStartPoints={setStartPoints}
+        changeTableData={changeTableData}
+      />
       <Button variant="primary">Primary</Button>
     </div>
   );
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,32 +2,55 @@ import React from "react";
 import { DAYS, ARRAY_STRUCTURE_TABLE } from "../../constants";
 import "./table.css";
 
-export default function Table({ tableData, toggleCell }) {
+export default function Table({
+  tableData,
+  isMouseDown,
+  setIsMouseDown,
+  startPoints,
+  setStartPoints,
+  changeTableData,
+}) {
   const createTable = () => {
     const dataTable = [];
-    for (let i = 0; i < ARRAY_STRUCTURE_TABLE.length; i++) {
-      const item = ARRAY_STRUCTURE_TABLE[i];
+    for (let day = 0; day < ARRAY_STRUCTURE_TABLE.length; day++) {
+      const item = ARRAY_STRUCTURE_TABLE[day];
       const row = [];
-      for (let j = 0; j < item.length; j++) {
-        const rowItem = item[j];
-        const id = j + 1 + DAYS[i];
+      for (let hour = 0; hour < item.length; hour++) {
+        const rowItem = item[hour];
+        const id = hour + 1 + DAYS[day];
         if (rowItem) {
           row.push(
-            <div className="cellColumTable" key={id} id={id}>
+            <div className="cellColumTable" key={id}>
               {rowItem}
             </div>
           );
         } else {
-          const isFree = tableData[DAYS[i]][j];
+          const isFree = tableData[DAYS[day]][hour];
           row.push(
             <div
               className={`cellItemTable ${isFree ? `white` : `red`}`}
-              day={DAYS[i]}
-              orderrow={i}
-              ordercell={j + 1}
               key={id}
-              id={id}
-              onClick={() => toggleCell(DAYS[i], j)}
+              onMouseDown={() => {
+                setIsMouseDown((prevState) => !prevState);
+                setStartPoints({
+                  startPoints: { day, hour },
+                  endPoints: { day, hour },
+                  isFree: !isFree,
+                });
+              }}
+              onMouseUp={() => {
+                setIsMouseDown((prevState) => !prevState);
+                setStartPoints({
+                  day: null,
+                  hour: null,
+                  isFree: true,
+                });
+              }}
+              onMouseEnter={() => {
+                if (isMouseDown) {
+                  changeTableData(day, hour);
+                }
+              }}
             >
               {rowItem}
             </div>
@@ -35,7 +58,7 @@ export default function Table({ tableData, toggleCell }) {
         }
       }
       dataTable.push(
-        <div className="rowTable" key={DAYS[i]}>
+        <div className="rowTable" key={DAYS[day]}>
           {row}
         </div>
       );
